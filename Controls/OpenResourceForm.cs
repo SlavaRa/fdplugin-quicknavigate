a48f309acd866cad656f1cc5d90d18f4d63ba5d2
slavara <ratdg@ya.ru>
2013-10-29 19:19:10 +0400

    -- minor refactor String->string, etc...

--- Controls/OpenResourceForm.cs
+++ Controls/OpenResourceForm.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 using PluginCore;
-using System.Runtime.CompilerServices;
 
 namespace QuickNavigatePlugin
 {
@@ -12,12 +11,12 @@ namespace QuickNavigatePlugin
     {
         private const int MAX_ITEMS = 100;
 
-        private List<String> projectFiles;
-        private List<String> openedFiles;
+        private List<string> projectFiles;
+        private List<string> openedFiles;
         private Font nameFont;
         private Font pathFont;
         private PluginMain plugin;
-        private String defaultMessage;
+        private string defaultMessage;
 
         public OpenResourceForm(PluginMain plugin)
         {
@@ -28,7 +27,6 @@ namespace QuickNavigatePlugin
                 Size = (plugin.Settings as Settings).ResourceFormSize;
 
             pathFont = new Font(listBox.Font.Name, listBox.Font.Size, FontStyle.Regular);
-            //nameFont = new Font(listBox.Font.Name, listBox.Font.Size, FontStyle.Regular);
             nameFont = new Font("Courier New", 10, FontStyle.Regular);
 
             defaultMessage = messageLabel.Text;
@@ -41,31 +39,22 @@ namespace QuickNavigatePlugin
         private void listBox_DrawItem(object sender, DrawItemEventArgs e)
         {
             if ((e.State & DrawItemState.Selected) == DrawItemState.Selected)
-            {
                 e.Graphics.FillRectangle(Brushes.LightSkyBlue, e.Bounds);
-            }
             else
-            {
                 e.Graphics.FillRectangle(new SolidBrush(listBox.BackColor), e.Bounds);
-            }
 
             if (e.Index >= 0)
             {
                 var fullName = (string) listBox.Items[e.Index];
 
                 int slashIndex = fullName.LastIndexOf('\\');
-                String path = " " + fullName.Substring(0, slashIndex + 1);
-                String name = fullName.Substring(slashIndex + 1);
+                string path = " " + fullName.Substring(0, slashIndex + 1);
+                string name = fullName.Substring(slashIndex + 1);
 
                 int pathSize = (int) e.Graphics.MeasureString(path, pathFont).Width - 2;
-                Rectangle pathBounds = e.Bounds;
-                var nameBounds = new Rectangle(
-                    e.Bounds.X + pathSize,
-                    e.Bounds.Y,
-                    e.Bounds.Width - pathSize,
-                    e.Bounds.Height);
+                var nameBounds = new Rectangle(e.Bounds.X + pathSize, e.Bounds.Y, e.Bounds.Width - pathSize, e.Bounds.Height);
 
-                e.Graphics.DrawString(path, pathFont, Brushes.Gray, pathBounds);
+                e.Graphics.DrawString(path, pathFont, Brushes.Gray, e.Bounds);
                 e.Graphics.DrawString(name, nameFont, Brushes.Black, nameBounds);
                 e.DrawFocusRectangle();
             }
@@ -78,43 +67,39 @@ namespace QuickNavigatePlugin
             fillListBox();
             if (listBox.Items.Count > 0)
                 listBox.SelectedIndex = 0;
+
             listBox.EndUpdate();
         }
 
         private void fillListBox()
         {
-            List<String> matchedItems;
+            List<string> matchedItems;
 
             if (textBox.Text.Length > 0)
             {
                 matchedItems = SearchUtil.getMatchedItems(openedFiles, textBox.Text, "\\", 0);
                 if (matchedItems.Capacity > 0)
                     matchedItems.Add("-----------------");
-                matchedItems.AddRange(
-                    SearchUtil.getMatchedItems(projectFiles, textBox.Text, "\\", MAX_ITEMS));
-            }
-            else
-            {
-                matchedItems = openedFiles;
-            }
 
-            foreach (String item in matchedItems)
-            {
-                listBox.Items.Add(item);
+                matchedItems.AddRange(SearchUtil.getMatchedItems(projectFiles, textBox.Text, "\\", MAX_ITEMS));
             }
+            else matchedItems = openedFiles;
+
+            foreach (string item in matchedItems)
+                listBox.Items.Add(item);
         }
 
         private void LoadFileList()
         {
-            openedFiles = new List<String>();
-            projectFiles = new List<String>();
+            openedFiles = new List<string>();
+            projectFiles = new List<string>();
             ShowMessage("Reading project files...");
             worker.RunWorkerAsync();
         }
 
         private void RebuildJob()
         {
-            List<String> allFiles = plugin.GetProjectFiles();
+            List<string> allFiles = plugin.GetProjectFiles();
 
             foreach (string file in allFiles)
             {
@@ -130,27 +115,25 @@ namespace QuickNavigatePlugin
 
         private bool isFileHidden(String file)
         {
-            String path = System.IO.Path.GetDirectoryName(file);
-            String name = System.IO.Path.GetFileName(file);
-            if (path.Contains(".svn") || path.Contains(".cvs") || path.Contains(".git"))
-                return true;
-            else if (name.Substring(0, 1) == ".")
-                return true;
-            else
-                return false;
+            string path = System.IO.Path.GetDirectoryName(file);
+            string name = System.IO.Path.GetFileName(file);
+            return path.Contains(".svn")
+                || path.Contains(".cvs")
+                || path.Contains(".git")
+                || name.Substring(0, 1) == ".";
         }
 
         private void Navigate()
         {
             if (listBox.SelectedItem != null)
             {
-                String file = PluginBase.CurrentProject.GetAbsolutePath((string)listBox.SelectedItem);
+                string file = PluginBase.CurrentProject.GetAbsolutePath((string)listBox.SelectedItem);
                 PluginBase.MainForm.OpenEditableDocument(file);
                 Close();
             }
         }
         
-        private void ShowMessage(String text)
+        private void ShowMessage(string text)
         {
             listBox.Items.Clear();
             listBox.Items.Add(text);
@@ -200,21 +183,16 @@ namespace QuickNavigatePlugin
         private void OpenResourceForm_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Escape)
-            {
                 Close();
-            }
             else if (e.KeyCode == Keys.Enter)
             {
                 e.Handled = true;
                 Navigate();
             }
-            else if (e.Control && e.KeyCode == Keys.R)
+            else if (e.Control && e.KeyCode == Keys.R && !worker.IsBusy)
             {
-                if (!worker.IsBusy)
-                {
-                    plugin.invalidateCache();
-                    LoadFileList();
-                }
+                plugin.invalidateCache();
+                LoadFileList();
             }
         }
 
