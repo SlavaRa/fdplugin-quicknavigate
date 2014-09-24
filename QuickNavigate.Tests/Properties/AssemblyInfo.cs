cc9c6cea5bcef43d7fb294c2f73f9f9be7ee7ac4
slavara <ratdg@ya.ru>
2014-09-24 17:15:07 +0400

    Reorganization

---
+++ QuickNavigate.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("QuickNavigate.Test")]
+[assembly: AssemblyDescription("Tests of QuickNavigate Plugin For FlashDevelop")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SlavaRa")]
+[assembly: AssemblyProduct("QuickNavigate.Test")]
+[assembly: AssemblyCopyright("FlashDevelop.org 2005-2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1b00ebd2-2225-46f5-acee-e266a6651a10")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
