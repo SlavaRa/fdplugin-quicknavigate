72992bcb5a2209aa97e585ea815359d3b2f5a2b3
SlavaRa <ratdg@ya.ru>
2015-11-21 03:23:53 +0300

    Add Resent Files Explorer

---
+++ QuickNavigate/Forms/RecentFilesExplorer.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace QuickNavigate.Forms
+{
+    partial class RecentFilesExplorer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.tree = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.input.BackColor = System.Drawing.SystemColors.Control;
+            this.input.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.input.Location = new System.Drawing.Point(13, 12);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(305, 21);
+            this.input.TabIndex = 1;
+            // 
+            // tree
+            // 
+            this.tree.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tree.BackColor = System.Drawing.SystemColors.Control;
+            this.tree.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tree.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tree.ItemHeight = 16;
+            this.tree.Location = new System.Drawing.Point(12, 40);
+            this.tree.Name = "tree";
+            this.tree.Size = new System.Drawing.Size(305, 173);
+            this.tree.TabIndex = 2;
+            // 
+            // RecentFilesExplorer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 225);
+            this.Controls.Add(this.tree);
+            this.Controls.Add(this.input);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(320, 200);
+            this.Name = "RecentFilesExplorer";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Recent Files";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.ListBox tree;
+    }
+}
