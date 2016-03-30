9007e69b85ec3542e088f9896a9cc00a4a0c9402
slavara <ratdg@ya.ru>
2016-03-30 08:05:19 +0300

    WIP

--- QuickNavigate/Forms/OpenRecentProjectsForm.cs
+++ QuickNavigate/Forms/OpenRecentProjectsForm.cs
@@ -4,7 +4,6 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
-using ASCompletion.Context;
 using JetBrains.Annotations;
 using PluginCore;
 using PluginCore.Helpers;
