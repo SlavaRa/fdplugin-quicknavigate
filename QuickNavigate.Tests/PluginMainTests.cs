331b8934e21d7aacd3708048f7d142246a3784c9
SlavaRa <ratdg@ya.ru>
2017-03-14 01:12:20 +0300

    [ci skip] Added PVS-Studio headers

--- QuickNavigate.Tests/PluginMainTests.cs
+++ QuickNavigate.Tests/PluginMainTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace QuickNavigate.Tests
 {
--- QuickNavigate.Tests/Properties/AssemblyInfo.cs
+++ QuickNavigate.Tests/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
